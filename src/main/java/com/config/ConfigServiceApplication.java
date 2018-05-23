4ecbfe8d0865af5a39609d01ac89f57a05749a6d
chengyouyuan <984415140@qq.com>
2018-05-23 10:02:43 +0800

    用于配置cloud的config项目

---
+++ src/main/java/com/config/ConfigServiceApplication.java
@@ -0,0 +1,14 @@
+package com.config;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServiceApplication {
+
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(ConfigServiceApplication.class).web(true).run(args);
+	}
+}
